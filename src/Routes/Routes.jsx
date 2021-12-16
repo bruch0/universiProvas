e490075fc0bd26d383d708477bbee20dc4d0ee64
bruch0 <lucas.bruch0@gmail.com>
2021-12-16 13:43:58 -0300

    feat: add component to the routes

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from '../Shared/GlobalStyles';
 import Home from '../Pages/Home';
 import Courses from '../Pages/Courses';
+import Professors from '../Pages/UniversityProfessors';
 
 function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:courseId/professors" element={<Professors />} />
       </Routes>
     </BrowserRouter>
   );
