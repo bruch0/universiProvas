0b51ff2f0d7c648e6a73882494eb96bb795a7801
bruch0 <lucas.bruch0@gmail.com>
2021-12-17 11:41:08 -0300

    feat: add subjects page

---
+++ src/Pages/CouseSubjects.jsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Loading from '../Components/Shared/Loading';
+import SmallUniversityDropdown from '../Components/Shared/UniversitySmallDropdown';
+import SmallTestFilter from '../Components/Shared/FilterSmallDropdown';
+import PeriodSubjects from '../Components/PeriodSubjects';
+
+import { getCouseSubject } from '../Services/api';
+import { getUniversityInfo } from '../Services/storage';
+
+function CourseSubjects() {
+  const [periodSubjects, setPeriodSubjects] = useState([]);
+  const [filteredPeriodSubjects, setFilteredPeriodSubjects] = useState([]);
+  const [filter, setFilter] = useState('');
+  const [courseName, setCourseName] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { id } = getUniversityInfo();
+  const { courseId } = useParams();
+
+  useEffect(() => {
+    getCouseSubject(id, courseId).then((response) => {
+      setPeriodSubjects(response.data.subjects);
+      setFilteredPeriodSubjects(response.data.subjects);
+      setCourseName(response.data.course.toUpperCase());
+      setLoading(false);
+    });
+  }, []);
+
+  if (loading) return <Loading />;
+
+  const filterSubjects = (search) => {
+    const filtered = [];
+    setFilter(search);
+
+    periodSubjects.forEach((period) => {
+      const auxFilter = [];
+
+      period.subjects.forEach((subject) => {
+        if (
+          subject.name.toLowerCase().startsWith(search.toLowerCase()) ||
+          subject.code.toLowerCase().startsWith(search.toLowerCase())
+        ) {
+          auxFilter.push(subject);
+        }
+      });
+
+      if (auxFilter.length) {
+        filtered.push({ period: period.period, subjects: auxFilter });
+      }
+    });
+    setFilteredPeriodSubjects(filtered);
+  };
+
+  return (
+    <ProfessorsPage>
+      <SmallUniversityDropdown />
+      <SmallTestFilter />
+      <Title>{courseName}</Title>
+      <FilterSearch
+        placeholder="Pesquise aqui..."
+        value={filter}
+        onChange={(e) => filterSubjects(e.target.value)}
+      />
+      {filter
+        ? filteredPeriodSubjects.map((period) => (
+            <PeriodSubjects period={period} key={period.period} />
+          ))
+        : periodSubjects.map((period) => (
+            <PeriodSubjects period={period} key={period.period} />
+          ))}
+    </ProfessorsPage>
+  );
+}
+
+export default CourseSubjects;
+
+const ProfessorsPage = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #cecfe5;
+  color: white;
+`;
+
+const Title = styled.p`
+  font-size: 6vw;
+  margin: 30px 0px 0px 0px;
+  padding: 0px 10%;
+  font-weight: 700;
+  text-align: center;
+
+  @media (max-width: 600px) {
+    font-size: 10vw;
+    margin: 20% 0px 0px 0px;
+  }
+`;
+
+const FilterSearch = styled.input`
+  width: 90%;
+  height: 50px;
+  padding: 0px 2%;
+  margin: 4% 0px 0px 0px;
+  font-size: 20px;
+  font-family: 'Quicksand';
+  border-radius: 15px;
+  border: 0px;
+`;
