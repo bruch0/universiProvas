9b22014fb194d74921fa0c535f480f9dfb4dd521
bruch0 <lucas.bruch0@gmail.com>
2021-12-18 10:20:36 -0300

    feat: change component filename and made it more reusable

---
+++ src/Components/SelectionGroup.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import SelectorButton from './Shared/SelectorButton';
+
+function SelectionGroup({ info, complement }) {
+  return (
+    <Period>
+      {`${info.period || info.type} ${complement}`}
+      <UniversityCourses>
+        {info.subjects
+          ? info.subjects.map((subject) => (
+              <SelectorButton
+                path="/"
+                mainInfo={subject.name}
+                additionalInfo={subject.code}
+                hoverInfo={subject.totalTests}
+                key={subject.code}
+              />
+            ))
+          : info.tests.map((test) => (
+              <SelectorButton
+                path=""
+                mainInfo={test.period}
+                hoverInfo="Abrir"
+                key={test.url}
+                action={() => window.open(test.url)}
+              />
+            ))}
+      </UniversityCourses>
+    </Period>
+  );
+}
+
+export default SelectionGroup;
+
+const Period = styled.section`
+  width: 100%;
+  padding: 0px 5%;
+  margin-top: 30px;
+  font-size: 30px;
+  font-weight: 500;
+`;
+
+const UniversityCourses = styled.section`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 6%;
+  margin-top: 2%;
+  padding: 0px 5%;
+
+  @media (max-width: 600px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
