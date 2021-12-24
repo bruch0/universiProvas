ff5e49e01b048aacb18da5b643126bbb012938bb
bruch0 <lucas.bruch0@gmail.com>
2021-12-24 16:04:33 -0300

    feat: add new logic, new input flow and new inputs

--- src/Pages/SubmitTest.jsx
+++ src/Pages/SubmitTest.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable no-restricted-syntax */
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
@@ -8,22 +6,32 @@ import styled from 'styled-components';
 import DropdownInput from '../Components/Shared/LargeDropdown';
 import Loading from '../Components/Shared/Loading';
 
-import { getPostTestInfo, uploadTest } from '../Services/api';
+import {
+  getUniversities,
+  getCourses,
+  getPostTestInfo,
+  uploadTest,
+} from '../Services/api';
 
 function SubmitTest() {
+  const [universities, setUniversities] = useState([]);
+  const [chosenUniversity, setChosenUniversity] = useState('');
+  const [universityId, setUniversityId] = useState(0);
+  const [courses, setCourses] = useState([]);
+  const [chosenCourse, setChosenCourse] = useState('');
+  const [courseId, setCourseId] = useState(0);
   const [professors, setProfessors] = useState([]);
-  const [choosedProfessor, setChoosedProfessor] = useState('');
+  const [chosenProfessor, setChosenProfessor] = useState('');
   const [subjects, setSubjects] = useState([]);
-  const [choosedSubject, setChoosedSubject] = useState('');
+  const [chosenSubject, setChosenSubject] = useState('');
   const [periods, setPeriods] = useState([]);
-  const [choosedPeriod, setChoosedPeriod] = useState('');
-  const [testType, setTestType] = useState([]);
-  const [choosedTestType, setChoosedTestType] = useState('');
+  const [chosenPeriod, setChosenPeriod] = useState('');
+  const [testTypes, setTestType] = useState([]);
+  const [chosenTestType, setChosenTestType] = useState('');
   const [testInfo, setTestInfo] = useState({
     professorId: 0,
     subjectId: 0,
     period: 0,
-    url: 0,
     typeId: 0,
     file: null,
   });
@@ -31,14 +39,60 @@ function SubmitTest() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    getPostTestInfo().then((response) => {
-      setProfessors(response.data.professors);
-      setSubjects(response.data.subjects);
-      setPeriods(response.data.availablePeriods);
-      setTestType(response.data.testTypes);
+    setLoading(true);
+    getUniversities().then((response) => {
+      setUniversities(response.data);
       setLoading(false);
     });
   }, []);
+
+  const resetTestInfo = (includingCourse) => {
+    if (includingCourse) {
+      setChosenCourse('');
+      setCourseId(0);
+    }
+    setChosenProfessor('');
+    setChosenSubject('');
+    setChosenPeriod('');
+    setChosenTestType('');
+    setTestInfo({
+      professorId: 0,
+      subjectId: 0,
+      period: 0,
+      typeId: 0,
+      file: null,
+    });
+  };
+
+  useEffect(() => {
+    if (universityId) {
+      resetTestInfo(true);
+      setLoading(true);
+      getCourses(universityId).then((response) => {
+        setCourses(response.data);
+        setLoading(false);
+      });
+    }
+  }, [universityId]);
+
+  useEffect(() => {
+    if (courseId) {
+      resetTestInfo(false);
+      setChosenProfessor('');
+      setChosenSubject('');
+      setChosenPeriod('');
+      setChosenTestType('');
+
+      setLoading(true);
+      getPostTestInfo(courseId).then((response) => {
+        setProfessors(response.data.professors);
+        setSubjects(response.data.subjects);
+        setPeriods(response.data.availablePeriods);
+        setTestType(response.data.testTypes);
+        setLoading(false);
+      });
+    }
+  }, [courseId]);
 
   if (loading) return <Loading />;
 
@@ -46,15 +100,15 @@ function SubmitTest() {
     e.preventDefault();
 
     const fileType = testInfo.file?.type.split('/')[1];
-    if (fileType !== 'pdf') {
-      Swal.fire('Insira um documento PDF');
-    } else if (
+    if (
       !testInfo.professorId ||
       !testInfo.subjectId ||
       !testInfo.typeId ||
       !testInfo.period
     ) {
       Swal.fire('Preencha todos os campos');
+    } else if (fileType !== 'pdf') {
+      Swal.fire('Insira um documento PDF');
     } else {
       const formData = new FormData();
       formData.append('file', testInfo.file);
@@ -72,40 +126,63 @@ function SubmitTest() {
 
   const inputs = [
     {
+      name: 'Universidade',
+      handler: (choice) => {
+        setChosenUniversity(choice.initials);
+        setUniversityId(choice.id);
+      },
+      state: chosenUniversity,
+      possibleStates: universities,
+    },
+    {
+      name: 'Curso',
+      handler: (choice) => {
+        setChosenCourse(choice.name);
+        setCourseId(choice.id);
+      },
+      state: chosenCourse,
+      possibleStates: courses,
+      disabled: Boolean(!chosenUniversity),
+    },
+    {
       name: 'Professor',
       handler: (choice) => {
         setTestInfo({ ...testInfo, professorId: choice.id });
-        setChoosedProfessor(choice.name);
+        setChosenProfessor(choice.name);
       },
-      state: choosedProfessor,
+      state: chosenProfessor,
       possibleStates: professors,
+      disabled: Boolean(!chosenCourse),
     },
     {
       name: 'Matéria',
       handler: (choice) => {
         setTestInfo({ ...testInfo, subjectId: choice.id });
-        setChoosedSubject(choice.name);
+        setChosenSubject(choice.name);
       },
-      state: choosedSubject,
+      state: chosenSubject,
       possibleStates: subjects,
+      disabled: Boolean(!chosenProfessor),
     },
     {
       name: 'Período',
       handler: (choice) => {
         setTestInfo({ ...testInfo, period: choice.name });
-        setChoosedPeriod(choice.name);
+        setChosenPeriod(choice.name);
       },
-      state: choosedPeriod,
+      state: chosenPeriod,
       possibleStates: periods,
+      disabled: Boolean(!chosenSubject),
     },
     {
       name: 'Prova',
       handler: (choice) => {
         setTestInfo({ ...testInfo, typeId: choice.id });
-        setChoosedTestType(choice.name);
+        setChosenTestType(choice.name);
       },
-      state: choosedTestType,
-      possibleStates: testType,
+      state: chosenTestType,
+      possibleStates: testTypes,
+      disabled: Boolean(!chosenPeriod),
     },
   ];
 
@@ -121,6 +198,7 @@ function SubmitTest() {
           handler={input.handler}
           state={input.state}
           possibleStates={input.possibleStates}
+          disabled={input.disabled}
           key={input.name}
         />
       ))}
@@ -181,6 +259,14 @@ const Holder = styled.form`
   display: flex;
   flex-direction: column;
   width: 20%;
+
+  @media (max-width: 1000px) {
+    width: 40%;
+  }
+
+  @media (max-width: 600px) {
+    width: 80%;
+  }
 `;
 
 const Label = styled.label`
@@ -194,6 +280,7 @@ const Label = styled.label`
   font-weight: 700;
   box-shadow: 0px 3px 10px rgb(0 0 0 / 25%);
   margin-bottom: 30px;
+  text-align: center;
   cursor: pointer;
 `;
 
@@ -212,6 +299,7 @@ const Send = styled.button`
   border: 0px;
   font-weight: 700;
   box-shadow: 0px 3px 10px rgb(0 0 0 / 25%);
+  margin-bottom: 30px;
   color: white;
   font-size: 25px;
   font-family: 'Quicksand';
